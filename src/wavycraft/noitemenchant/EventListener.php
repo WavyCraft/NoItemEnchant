36d60d1b372189edf07b5ed03187ee635c3fc57c
WavyCraft <wavycraftnetwork@gmail.com>
2025-06-07 22:19:59 -0700

    Update EventListener.php

--- src/wavycraft/noitemenchant/EventListener.php
+++ src/wavycraft/noitemenchant/EventListener.php
@@ -11,15 +11,15 @@ use pocketmine\item\StringToItemParser;
 
 class EventListener implements Listener {
 
-    public function cancelEnchant(PlayerItemEnchantEvent $event) : void{
-        $item = $event->getItem();
+    public function onEnchant(PlayerItemEnchantEvent $event) : void{
+        $item = $event->getInputItem();
 
-        foreach (NoItemEnchant::$blocked_items as $blocked_item) {
-            $parsed_item = StringToItemParser::getInstance()->parse($blocked_item);
-            if ($parsed_item !== null && $parsed_item->getTypeId() === $item->getTypeId()) {
+        foreach (NoItemEnchant::$blocked_items as $blockedItem) {
+            $parsedItem = StringToItemParser::getInstance()->parse($blockedItem);
+            if ($parsedItem !== null && $parsedItem->getTypeId() === $item->getTypeId()) {
                 $event->cancel();
-                $event->getPlayer()->sendMessage("§eYou cannot enchant this item: §c" . $parsed_item->getVanillaName());
-                break;
+                $event->getPlayer()->sendMessage("§eYou cannot enchant this item: §c" . $parsedItem->getVanillaName());
+                return;
             }
         }
     }
